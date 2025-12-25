2153724960a4ee6dd4c9c5b5708117eb27248d52
Vapid33 <297141009@qq.com>
2025-12-25 19:42:33 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -361,7 +361,7 @@ export default function WorkflowPage() {
   const fetchTransactions = async (page: number) => {
     try {
       const res = await fetch(
-        "http://172.20.10.6:8088//admin/queryOrderList",
+        "http://172.20.10.6:8088/admin/queryOrderList",
         {
           method: "POST",
           headers: {
